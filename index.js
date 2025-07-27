cb084b3138e855775a6d7d4670035dd7c5a22e6f
neshadx <neshadimagify@gmail.com>
2025-07-27 22:45:51 +0600

    added some code

--- index.js
+++ index.js
@@ -252,11 +252,21 @@ const path = require("path");
 require("dotenv").config();
 
 //  Firebase Admin Initialization
-const serviceAccount = require("./firebase-service-account.json");
+// const serviceAccount = require("./firebase-service-account.json");
 
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
 });
+
+
+
 
 const app = express();
 const port = process.env.PORT || 5000;
